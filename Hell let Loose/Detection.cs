20649c69c0f424a214d3270ec7bc99830078ba89
clegast <clegast512@gmail.com>
2022-11-10 11:41:01 +0100

    clean up Detection

--- Hell let Loose/Detection.cs
+++ Hell let Loose/Detection.cs
@@ -1,81 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO.Ports;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using IronOcr;
-//using static System.Windows.Forms.AxHost;
-using System.Runtime.InteropServices;
-using MessageBox = System.Windows.MessageBox;
-using Newtonsoft.Json.Linq;
-//using Twilio.TwiML.Voice;
-using static System.Net.Mime.MediaTypeNames;
-using System.Windows.Media.Media3D;
 using System.Threading;
 
 namespace Main
 {
-    internal static class Detection
+  internal static class Detection
+  {
+
+    public static Coordinates setPosition(int cordx, int cordy)
     {
-        //function that searches for a certain color on the screen
-        public static Coordinates checkMarker(Bitmap Marker)
-        {
-            int kordx = 0;
-            int kordy = 0;
-            Bitmap bmpMarker = Marker;
-            string[] Comparison = { "ffffff", "fffffe" };
-            while (kordy < bmpMarker.Height)
-            {
-                Color pixelColor = bmpMarker.GetPixel(kordx, kordy);
-
-                for (int i = 0; i < Comparison.Length; i++)
-                {
-
-                }
-            }
-            Coordinates setCoordinates = new Coordinates(kordx, kordy);
-            return setCoordinates;
-        }
-
-        public static Coordinates setPosition(int cordx, int cordy)
-        {
-            Coordinates coordinates = new Coordinates(cordx, cordy);
-            return coordinates;
-        }
+      Coordinates coordinates = new Coordinates(cordx, cordy);
+      return coordinates;
+    }
 
     //function that crops the screenshot and then converts the number to an Integer
     internal static int Imgtotxt(int x, int y, int Width, int Height, int last)
     {
       string pathCropped = Directory.GetCurrentDirectory() + "Screenshot.png";
-     
+
       InputConvert inputconvert = new InputConvert();
 
-            Imgediting.CropScreenshot(x, y, Width, Height).Save(pathCropped);
+      Imgediting.CropScreenshot(x, y, Width, Height).Save(pathCropped);
 
       var varResult = new IronTesseract().Read(pathCropped);
       string strResult = varResult.Text;
       int intResult = inputconvert.Convert(strResult, last);
 
-            File.Delete(pathCropped);
-            return intResult;
-        }
+      File.Delete(pathCropped);
+      return intResult;
+    }
 
-        public static List<Suspeced> Detecte()
-        {
-            Bitmap OldScreenshot = null;
-            Bitmap NewScreenshot = null;
-            
-            //Imgediting imgediting = new Imgediting();
+    public static List<Suspeced> Detecte()
+    {
+      Bitmap OldScreenshot = null;
+      Bitmap NewScreenshot = null;
 
-            OldScreenshot = Imgediting.CropScreenshot(695, 268, 528, 528);
+      //Imgediting imgediting = new Imgediting();
 
-            Thread.Sleep(10000);
-            NewScreenshot = Imgediting.CropScreenshot(695, 268, 528, 528);
-            //NewScreenshot.Save("C:\\Users\\linus\\Desktop\\test2.png");
+      OldScreenshot = Imgediting.CropScreenshot(695, 268, 528, 528);
+
+      Thread.Sleep(10000);
+      NewScreenshot = Imgediting.CropScreenshot(695, 268, 528, 528);
+      //NewScreenshot.Save("C:\\Users\\linus\\Desktop\\test2.png");
 
       List<Suspeced> Posiblemarkers = new List<Suspeced>();
       for (int Y = 0; Y < NewScreenshot.Height; Y++)
@@ -107,32 +75,32 @@ namespace Main
         }
       }
       Thread.Sleep(500);
-      
+
       return Posiblemarkers;
 
 
-        }
-        public static Coordinates GetCoordinatsfromList(List<Suspeced> Markers)
-        {
-            Suspeced suspeced = Markers[0];
-            Coordinates coordinates = suspeced.GetTargetCoordinates();
-            coordinates.xcordinate += 695;
-            coordinates.ycordinate += 268;
-            return coordinates;
-        }
-
-
-        public static Coordinates GetTarget()
-        {
-            List<Suspeced> suspeceds = new List<Suspeced>();
-            do
-            {
-                suspeceds = Detecte();
-                System.GC.Collect();
-
-
-            } while (suspeceds.Count == 0);
-            return GetCoordinatsfromList(suspeceds);
-        }
     }
+    public static Coordinates GetCoordinatsfromList(List<Suspeced> Markers)
+    {
+      Suspeced suspeced = Markers[0];
+      Coordinates coordinates = suspeced.GetTargetCoordinates();
+      coordinates.xcordinate += 695;
+      coordinates.ycordinate += 268;
+      return coordinates;
+    }
+
+
+    public static Coordinates GetTarget()
+    {
+      List<Suspeced> suspeceds = new List<Suspeced>();
+      do
+      {
+        suspeceds = Detecte();
+        System.GC.Collect();
+
+
+      } while (suspeceds.Count == 0);
+      return GetCoordinatsfromList(suspeceds);
+    }
+  }
 }
