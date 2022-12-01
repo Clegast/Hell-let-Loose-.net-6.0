df3355465be9e6f0d9649c8d935c2b9da5ff599d
clegast <clegast512@gmail.com>
2022-12-01 10:51:16 +0100

    ..

---
+++ Hell let Loose/Temp.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hell_let_Loose
+{
+  internal class Temp
+  {
+    public static int getNumber(Bitmap Number)
+    {
+
+
+      return 0;
+    }
+
+  }
+}
