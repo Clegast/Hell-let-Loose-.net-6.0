ca6f549cc4841714ccd5d19e0da8648da218ec05
clegast <clegast512@gmail.com>
2022-12-01 12:38:28 +0100

    Prototyp getNumber

--- Hell let Loose/Temp.cs
+++ Hell let Loose/Temp.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -16,46 +15,46 @@ namespace Hell_let_Loose
 
       if (RGBfilter.IsNotBackgroundNoice(ColorExpected, Number.GetPixel(0, 3), 10))
       {
-        //number 9, 0, 6, 5 or 8
+        //number 9, 6, 5 or 8
         if (RGBfilter.IsNotBackgroundNoice(ColorExpected, Number.GetPixel(2, 5), 10))
         {
-          return "9";
+          if (RGBfilter.IsNotBackgroundNoice(ColorExpected, Number.GetPixel(3,2), 10))
+            return "9";
+          else
+            return "4";
         }
         else
         {
-          //number 0, 6, 5 or 8
+          //number 6, 5 or 8
           if (RGBfilter.IsNotBackgroundNoice(ColorExpected, Number.GetPixel(3, 0), 10))
-          {
             return "5";
-          }
           else
           {
             if (RGBfilter.IsNotBackgroundNoice(ColorExpected, Number.GetPixel(3, 3), 10))
-            {
-              if (RGBfilter.IsNotBackgroundNoice(ColorExpected, Number.GetPixel(3, 4), 10))
-              {
-                return "0";
-              }
-              else
-              {
-                return "8";
-              }
-            }
+              return "8";
             else
-            {
               return "6";
-            }
           }
         }
       }
       else
       {
-        //numper 1, 2, 3, 4 or 7
+        //numper 1, 2, 3 or 7
+        if (RGBfilter.IsNotBackgroundNoice(ColorExpected, Number.GetPixel(0, 1), 10))
+          return "1";
+        else
+        {
+          if (RGBfilter.IsNotBackgroundNoice(ColorExpected, Number.GetPixel(0, 0), 10))
+            return "7";
+          else
+          {
+            if (RGBfilter.IsNotBackgroundNoice(ColorExpected, Number.GetPixel(4, 6), 10))
+              return "3";
+            else
+              return "2";
+          }
+        }
       }
-
-
-      return "";
     }
-
   }
 }
