421cfe5a8944d4a9ae3a29d41a2a813c9666ca35
clegast <clegast512@gmail.com>
2022-12-01 12:03:30 +0100

    add GetNumber

--- Hell let Loose/Temp.cs
+++ Hell let Loose/Temp.cs
@@ -10,11 +10,51 @@ namespace Hell_let_Loose
 {
   internal class Temp
   {
-    public static int getNumber(Bitmap Number)
+    public static string getNumber(Bitmap Number)
     {
+      Color ColorExpected = Color.White;
+
+      if (RGBfilter.IsNotBackgroundNoice(ColorExpected, Number.GetPixel(0, 3), 10))
+      {
+        //number 9, 0, 6, 5 or 8
+        if (RGBfilter.IsNotBackgroundNoice(ColorExpected, Number.GetPixel(2, 5), 10))
+        {
+          return "9";
+        }
+        else
+        {
+          //number 0, 6, 5 or 8
+          if (RGBfilter.IsNotBackgroundNoice(ColorExpected, Number.GetPixel(3, 0), 10))
+          {
+            return "5";
+          }
+          else
+          {
+            if (RGBfilter.IsNotBackgroundNoice(ColorExpected, Number.GetPixel(3, 3), 10))
+            {
+              if (RGBfilter.IsNotBackgroundNoice(ColorExpected, Number.GetPixel(3, 4), 10))
+              {
+                return "0";
+              }
+              else
+              {
+                return "8";
+              }
+            }
+            else
+            {
+              return "6";
+            }
+          }
+        }
+      }
+      else
+      {
+        //numper 1, 2, 3, 4 or 7
+      }
 
 
-      return 0;
+      return "";
     }
 
   }
