cd5a5093bc6383c8d15cb5dec97451af915b0069
clegast <clegast512@gmail.com>
2022-12-08 21:08:49 +0100

    test

--- Hell let Loose/Detection.cs
+++ Hell let Loose/Detection.cs
@@ -114,7 +114,8 @@ namespace Main
 
       for(int i = 0; i < Screenshot.Width; i++)
       {
-        if (Screenshot.GetPixel(i, 33).R == 2 && Screenshot.GetPixel(i, 33).G == 113 && Screenshot.GetPixel(i, 33).B == 187)
+        //if (Screenshot.GetPixel(i, 33).R == 2 && Screenshot.GetPixel(i, 33).G == 113 && Screenshot.GetPixel(i, 33).B == 187)
+        if (RGBfilter.IsAproximatlyCollor(Screenshot.GetPixel(i, 33), 2, 113, 187, 10))
         {
           return true;
         }
